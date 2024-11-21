bce95ba5a3c60d84e11a4041c5fbb6e0db5c161d
ksw2370040 <ksw2370040@stu.o-hora.ac.jp>
2024-11-21 10:32:21 +0900

    AWS

--- AR_app/config.js
+++ AR_app/config.js
@@ -3,12 +3,13 @@ const { Client } = require('pg');
 require('dotenv').config();
 
 const connection = new Client({
-    user: process.env.DB_USER || 'postgres',
-    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'postgres',  // DB_USERの設定も確認
+    host: process.env.DB_HOST || 'tabitomo-db.crgucea6c04o.us-east-1.rds.amazonaws.com', // RDSのホスト名
     database: process.env.DB_NAME || 'tabitomo',
-    password: process.env.DB_PASSWORD || 'kashi0001',
+    password: process.env.DB_PASSWORD || 'kashi0001',  // DB_PASSWORDの設定も確認
     port: process.env.DB_PORT || 5432,
 });
+
 
 connection.connect()
     .then(() => console.log('PostgreSQLデータベースに接続しました'))
