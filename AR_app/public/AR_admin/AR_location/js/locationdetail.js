8d4de31afce3b150592c62e65c1d16a0459dc9fc
ksw2370040 <ksw2370040@stu.o-hora.ac.jp>
2025-01-28 12:37:02 +0900

    a

--- AR_app/public/AR_admin/AR_location/js/locationdetail.js
+++ AR_app/public/AR_admin/AR_location/js/locationdetail.js
@@ -49,9 +49,9 @@ document.addEventListener('DOMContentLoaded', function () {
                             <a href="../../Content/markerimage/${model.mkimage}" download="${model.mkimage}" class="download-btn">
                                 <button>ダウンロード</button>
                             </a><br>
-                            <strong><a href="../../AR_admin/AR_napisy/napisylist.html?mdltext=${model.mdltext}">音声ファイル</a></strong><br>
+                            <strong><a href="../../AR_admin/AR_napisy/napisylist.html?mdltext=${model.mdltext}">テキストファイル</a></strong><br>
                             ${model.textfiles.length > 0 ? model.textfiles.map(file => `<span>${file}</span><br>`).join('') : 'なし'}<br>
-                            <strong><a href="../../AR_admin/AR_sound/soundlist.html?mdlsound=${model.mdlsound}">テキストファイル</a></strong><br>
+                            <strong><a href="../../AR_admin/AR_sound/soundlist.html?mdlsound=${model.mdlsound}">音声ファイル</a></strong><br>
                             ${model.soundfiles.length > 0 ? model.soundfiles.map(file => `<span>${file}</span><br>`).join('') : 'なし'}
                         </p>
                         <button class="edit-btn" data-mdlid="${mdlid}">編集</button>
