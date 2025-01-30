9a6d45a4e69ca029152241c94dac32f96a194eea
ksw2370040 <ksw2370040@stu.o-hora.ac.jp>
2025-01-30 11:41:54 +0900

    a

--- AR_app/routes/multiAR.js
+++ AR_app/routes/multiAR.js
@@ -8,13 +8,15 @@ router.get('/', async (req, res) => {
 
   try {
     const query = `
-      SELECT 
-        mdlID, mkname, patt, mkimage, mdlname, mdlimage, mdlsound, mdltext 
-      FROM MODEL2
-      JOIN sound ON MODEL2.mdlsound = sound.mdlsound
-      JOIN napisy ON MODEL2.mdltext = napisy.mdltext
-      WHERE sound.languagename = $1 AND napisy.languagename = $1
-    `;
+    SELECT 
+      mdl2.mdlID, mdl2.mkname, mdl2.patt, mdl2.mkimage, mdl2.mdlname, mdl2.mdlimage, 
+      sound.mdlsound, napisy.mdltext
+    FROM MODEL2 mdl2
+    JOIN sound ON mdl2.mdlsound = sound.mdlsound
+    JOIN napisy ON mdl2.mdltext = napisy.mdltext
+    WHERE sound.languagename = $1 AND napisy.languagename = $1
+  `;
+  
     const result = await connection.query(query, [languagename]);
 
     const markers = result.rows.map(row => ({
